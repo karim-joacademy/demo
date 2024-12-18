2ae8dbad23661ab548d960e9ce34c0c11b71fae0
karim.joacademy <karim.joacademy@gmail.com>
2024-12-18 16:46:35 +0300

    testing

--- index.php
+++ index.php
@@ -2,12 +2,19 @@
 
 require "utils/functions.php";
 require "utils/Database.php";
-//require "utils/router.php";
+require "utils/router.php";
 
 $config = require "utils/config.php";
 
 $db = new Database($config["database"]);
 
-$posts = $db->query("SELECT id FROM posts")->fetchAll(PDO::FETCH_ASSOC);
+$id = $_GET["id"];
+
+$getAllQuery = "SELECT id FROM users";
+$getByIdQuery = "SELECT * FROM users WHERE id = :id";
+
+$posts = $db->query($getAllQuery)->fetchAll(PDO::FETCH_ASSOC);
+//$post = $db->query($getByIdQuery, [":id" => $id])->fetch(PDO::FETCH_ASSOC);
 
 Dump_Die($posts);
+//Dump_Die($post);
